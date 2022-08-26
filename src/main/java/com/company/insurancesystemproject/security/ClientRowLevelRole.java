06ab3a074dbc12b0e0d49615680984dacc0555ed
ChernPaul <65501154+ChernPaul@users.noreply.github.com>
2022-08-26 18:16:49 +0400

    Update ClientRowLevelRole.java

--- src/main/java/com/company/insurancesystemproject/security/ClientRowLevelRole.java
+++ src/main/java/com/company/insurancesystemproject/security/ClientRowLevelRole.java
@@ -2,11 +2,9 @@ package com.company.insurancesystemproject.security;
 
 
 import com.company.insurancesystemproject.entity.Agreement;
-import com.company.insurancesystemproject.entity.User;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.security.model.RowLevelBiPredicate;
 import io.jmix.security.model.RowLevelPolicyAction;
-import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
 import io.jmix.security.role.annotation.PredicateRowLevelPolicy;
 import io.jmix.security.role.annotation.RowLevelRole;
 import org.springframework.context.ApplicationContext;
@@ -15,20 +13,15 @@ import org.springframework.context.ApplicationContext;
         name = "clientRowLevel",
         code = "client-row-level-role")
 public interface ClientRowLevelRole {
-    @JpqlRowLevelPolicy(
-            entityClass = Agreement.class,
-            where = "{E}.client.user = :current_user")
-
-    void agreement();
-
     @PredicateRowLevelPolicy(
-            entityClass = User.class,
+            entityClass = Agreement.class,
             actions = { RowLevelPolicyAction.READ})
-    default RowLevelBiPredicate<User, ApplicationContext> readMyUserOnly() {
-        return (user, applicationContext) -> {
+    default RowLevelBiPredicate<Agreement, ApplicationContext> readMyAgreements() {
+        return (agreement, applicationContext) -> {
             CurrentAuthentication currentAuthentication = applicationContext.getBean(CurrentAuthentication.class);
-            return user.equals(currentAuthentication.getUser());
+            return agreement.getClient().getUser().equals(currentAuthentication.getUser());
         };
     }
 
 }
+
